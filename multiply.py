8a78c3d5570d6a0a9d1589ab42de67e510a33348
Matthew Meyers <jedimastermeyers@gmail.com>
2020-01-07 12:07:32 -0700

    finished first assignments

--- multiply.py
+++ multiply.py
@@ -1,19 +1,49 @@
+#first matix must have the same number of columns as the second matrix has rows
+#if this isn't true it is an invalid matrix multiplication
+
 print('Enter a matrix a row at a time. End with a blank line.')
-data = input(': ')
+userInput = '0'
+matrixOne = []
 
-while data:
-    matrixOne = [data]
-    data = input(': ')
-    if data:
-        matrixOne.append(data)
+while userInput:
+    userInput = input(': ')
+    if userInput:
+        nums = list(map(int, userInput.split()))
+        matrixOne.append(nums)
 
-print('Enter a matrix. End with a blank line.')
-data = input(': ')
+print('\nEnter a matrix. End with a blank line.')
+userInput = '0'
+matrixTwo = []
 
-while data:
-    matrixTwo = [data]
-    data = input(': ')
-    if data:
-        matrixTwo.append(data)
+while userInput:
+    userInput = input(': ')
+    if userInput:
+        nums = list(map(int, userInput.split()))
+        matrixTwo.append(nums)
 
-length = len(matrixOne)
+if len(matrixOne) < 1 or len(matrixOne[0]) != len(matrixTwo):
+    print('\nProvided matrices are not compatible.')
+else:
+    finalMatrix = []
+
+    i = 0
+    while i < len(matrixOne):
+        row = []
+        j = 0
+        while j < len(matrixTwo[0]):
+            k = 0
+            while k < len(matrixTwo):
+                result = matrixOne[i][k] * matrixTwo[k][j]
+                k += 1
+                result2 = matrixOne[i][k] * matrixTwo[k][j]
+                row.append(result + result2)
+                k += 1
+            j += 1
+        i += 1
+        finalMatrix.append(row)
+
+    print('\nResult:')
+    for item in finalMatrix:
+        for num in item:
+            print('{:>4}'.format(str(num)), end= '')
+        print('')
