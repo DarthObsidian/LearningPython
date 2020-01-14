3b16be8a8a87736f15a55802948ed1dfd019ec13
Matthew Meyers <jedimastermeyers@gmail.com>
2020-01-13 17:28:39 -0700

    worked on second assignment

--- Homework2/queries.py
+++ Homework2/queries.py
@@ -1,5 +1,12 @@
 import parts
+boltSupps = {s[1] for s in parts.suppliers
+                  for r in parts.spj
+                  for p in parts.parts
+                  if r[0] == s[0] if p[0] == r[1] if p[1] == 'Bolt'}
+print(boltSupps)
 
-boltSup = {}
-redSup = {}
-cityPairs = {}
+colorSupps = {s[1] for s in parts.suppliers
+                   for r in parts.spj
+                   for p in parts.parts
+                   if r[0] == s[0] if p[0] == r[1] if p[2] == 'Red'}
+print(colorSupps)
