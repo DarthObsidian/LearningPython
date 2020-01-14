453b92d170c6038395871660fc939665df4aa521
Matthew Meyers <jedimastermeyers@gmail.com>
2020-01-13 17:58:24 -0700

    worked on second assignment

--- Homework2/queries.py
+++ Homework2/queries.py
@@ -10,3 +10,8 @@ colorSupps = {s[1] for s in parts.suppliers
                    for p in parts.parts
                    if r[0] == s[0] if p[0] == r[1] if p[2] == 'Red'}
 print(colorSupps)
+
+sameCitySupps = {(s[1], d[1]) for s in parts.suppliers
+                              for d in parts.suppliers
+                              if d[3] == s[3] and d[1] != s[1]}
+print(sameCitySupps)
