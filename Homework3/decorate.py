8f8a96c0da10d395cdc5691a1c2b13ce2a193333
Matthew Meyers <jedimastermeyers@gmail.com>
2020-01-27 17:44:43 -0700

    worked on third assignment

--- Homework3/decorate.py
+++ Homework3/decorate.py
@@ -1,16 +1,35 @@
-def track(f) :
+import sys
+
+def track(f):
     def wrapper (*args, **kwargs):
-        wrapper.count += 1
-        if str(args)+str(kwargs) in wrapper.results:
-            print(str(args)+str(kwargs), 'found in cache')
-        wrapper.results[str(args) + str(kwargs)] = {}
-        return f(*args, **kwargs)
+        key = str(args)+str(kwargs)
+        if key in results:
+            print(key, 'found in cache')
+            return results[key]
+        else:
+            wrapper.count += 1
+            val = f(*args, **kwargs)
+            results[key] = val
+            return val
     wrapper.count = 0
-    wrapper.results = dict()
+    results = {}
     return wrapper
 
-@track
-def fib(n):
-    return n if n in (0,1) else fib(n-1) + fib(n-2)
+def log(f):
+    def wrapper (*args, **kwargs):
+        logfile = open(sys.path[0] + '\\log.txt', 'a')
+        val = f(*args, **kwargs)
+        logfile.write(str(args)+str(kwargs) + ' = ' + str(val) + '\n')
+        logfile.close()
+        return val
+    return wrapper
 
-print(str(fib(10))+',', 'calls =', fib.count)
+def main():
+    @track
+    @log
+    def fib(n):
+        return n if n in (0,1) else fib(n-1) + fib(n-2)
+    print(str(fib(10))+',', 'calls =', fib.count)
+
+if __name__ == "__main__":
+    main()
