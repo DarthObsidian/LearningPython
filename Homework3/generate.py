23f361e867945a8e7c67bbf5b95731e58efbd52f
Matthew Meyers <jedimastermeyers@gmail.com>
2020-01-28 17:34:18 -0700

    Finished third assignment

--- Homework3/generate.py
+++ Homework3/generate.py
@@ -1,7 +1,29 @@
 def produce (input):
-    return ''
+    i = 0
+    while i < len(input):
+        if(input[i].isdigit()):
+            yield input[i+1]*(int(input[i]) + 1)
+            i += 2
+        else:
+            yield input[i]
+            i += 1
+
+def consume(generator):
+    result = ''
+    for word in generator:
+        for char in word:
+            result += char
+            if (len(result) + 1) % 4 == 0:
+                result += ' '
+    print(result)
 
 def main():
     p = produce('A2B5E3426FG0ZYW3210PQ89R')
     for s in p:
         print(s, end=' ')
+    print()
+
+    consume(produce('A2B5E3426FG0ZYW3210PQ89R'))
+
+if __name__ == "__main__":
+    main()
