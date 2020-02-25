f18b75f4583df992d87482039ecb95fe5e470fc7
Matthew Meyers <jedimastermeyers@gmail.com>
2020-02-25 12:58:58 -0700

    Finished sixth assignment

--- Homework6/view.py
+++ Homework6/view.py
@@ -1,34 +1,53 @@
 import sys
-
+import os
 
 def view(fileName, view_size=25):
-    offsets = [0]
-   
+    #check to see if the file is empty
+    if os.path.getsize(fileName) < 1:
+        print("Error: specified file is empty")
+        return
+
     with open(fileName, 'r') as f:
+        #save positions into a list
+        offsets = [0]
         i = 0
         while f.readline():
             i += 1
             if i % view_size == 0:
+                f.seek(f.tell())
                 offsets.append(f.tell())
+        f.seek(0,2)
+        fileEnd = f.tell()
 
         command = ''
         currentPos = 0
         while command != 'q':
-            result = ''
+            result = []
+            #move to the current position
             f.seek(offsets[currentPos])
-            print(f'starting at pos {f.tell()} in {currentPos}/{len(offsets)}')
-            while f.tell() != offsets[currentPos + 1]:
-                result += f.readline()
-            print(result)
-            print(f'ending at {f.tell()}')
+            
+            nextPos = currentPos + 1
+            if nextPos == len(offsets):
+                nextPos = 0
 
+            #add lines into a list 
+            while f.tell() != offsets[nextPos] and f.tell() != fileEnd:
+                result.append(f.readline())
+
+            #if the list of lines is too short, lengthen it
+            while len(result) < view_size:
+                result.append('\n')
+
+            print(''.join(result))
+            
             command = input('Command [u,d,t,b,#,q]: ')
 
+            #change the position based on user input
             if command == 'u':
                 currentPos -= 1
                 if currentPos < 0:
                     currentPos = len(offsets) - 1
-            elif command == 'd':
+            elif command == 'd' or command == '':
                 currentPos += 1
                 if currentPos >= len(offsets):
                     currentPos = 0
@@ -36,10 +55,11 @@ def view(fileName, view_size=25):
                 currentPos = 0
             elif command == 'b':
                 currentPos = len(offsets) - 1
-            elif command.isdigit:
+            elif command.isdigit():
                 currentPos = int(command)
             elif command != 'q':
                 print("Error: Invalid command")
+            print()
 
 def main():
     if len(sys.argv) > 2:
