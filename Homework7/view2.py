fd5abf737f57545097736d62c62c1dc8876758e0
Matthew Meyers <jedimastermeyers@gmail.com>
2020-03-05 12:05:50 -0700

    Finished seventh assignment

--- Homework7/view2.py
+++ Homework7/view2.py
@@ -3,10 +3,8 @@ import os
 import tkinter as tk
 import tkinter.simpledialog
 
-currentPos = 0
 
 def view(fileName, view_size=25):
-    window(view_size)
     #check to see if the file is empty
     if os.path.getsize(fileName) < 1:
         print("Error: specified file is empty")
@@ -15,6 +13,9 @@ def view(fileName, view_size=25):
     with open(fileName, 'r') as f:
         #save positions into a list
         offsets = [0]
+        currentPos = 0
+
+        #get the offsets
         i = 0
         while f.readline():
             i += 1
@@ -24,11 +25,12 @@ def view(fileName, view_size=25):
         f.seek(0,2)
         fileEnd = f.tell()
 
-        command = ''
-        global currentPos
-        while command != 'q':
+        #update the text in the window to be the current page
+        def updateText(tkText, window):
             result = []
             #move to the current position
+            nonlocal offsets
+            nonlocal currentPos
             f.seek(offsets[currentPos])
             
             nextPos = currentPos + 1
@@ -43,62 +45,59 @@ def view(fileName, view_size=25):
             while len(result) < view_size:
                 result.append('\n')
 
-            print(''.join(result))
+            #update the tkinter textbox
+            text.config(state=tk.NORMAL)
+            tkText.delete('1.0', tk.END)
+            tkText.insert('1.0', ''.join(result))
+            text.config(state=tk.DISABLED)
             
-            command = input('Command [u,d,t,b,#,q]: ')
+            #update window title
+            window.title(f.name + ' - Page ' + str(currentPos+1))
 
-            #change the position based on user input
-            if command == 'u':
-                currentPos -= 1
-                if currentPos < 0:
-                    currentPos = len(offsets) - 1
-            elif command == 'd' or command == '':
-                currentPos += 1
-                if currentPos >= len(offsets):
-                    currentPos = 0
-            elif command == 't':
-                currentPos = 0
-            elif command == 'b':
+        #change the page number to be the given page
+        def changePageNumber(num, tkText, window):
+            nonlocal currentPos
+            currentPos = num
+            if currentPos < 0:
                 currentPos = len(offsets) - 1
-            elif command.isdigit():
-                currentPos = int(command)
-            elif command != 'q':
-                print("Error: Invalid command")
-            print()
+            elif currentPos >= len(offsets):
+                currentPos = 0
+            updateText(tkText, window)
 
-def window(view_size):
-    mainWindow = tk.Tk()
-    scrollbar = tk.Scrollbar(orient=tk.HORIZONTAL)
-    scrollbar.pack(side=tk.BOTTOM, fill='x')
+        #change the page number to one given by the user
+        def getPageNumber(tkText, window):
+            changePageNumber(tk.simpledialog.askinteger('Page Number', 'Enter page number')-1, tkText, window)
+        
+        #create the window
+        mainWindow = tk.Tk()
+        scrollbar = tk.Scrollbar(orient=tk.HORIZONTAL)
+        scrollbar.pack(side=tk.BOTTOM, fill='x')
 
-    text = tk.Text(height=view_size, width=50, wrap=tk.NONE, xscrollcommand=scrollbar.set)
-    text.pack()
+        text = tk.Text(height=view_size, width=50, wrap=tk.NONE, xscrollcommand=scrollbar.set)
+        text.pack()
+        updateText(text, mainWindow)
 
-    scrollbar.config(command=text.xview)
+        scrollbar.config(command=text.xview)
 
-    buttonFrame=tk.Frame(mainWindow)
-    buttonFrame.pack(side=tk.BOTTOM)
+        buttonFrame=tk.Frame(mainWindow)
+        buttonFrame.pack(side=tk.BOTTOM)
 
-    bWidth = 8
-    pad=2
-    top = tk.Button(buttonFrame, text='TOP', width=bWidth)
-    top.grid(column=0, row=1, padx=pad)
-    up = tk.Button(buttonFrame, text='UP', width=bWidth)
-    up.grid(column=1, row=1, padx=pad)
-    down = tk.Button(buttonFrame, text='DOWN', width=bWidth)
-    down.grid(column=2, row=1, padx=pad)
-    bottom = tk.Button(buttonFrame, text='BOTTOM', width=bWidth)
-    bottom.grid(column=3, row=1, padx=pad)
-    page = tk.Button(buttonFrame, text='PAGE', width=bWidth)
-    page.grid(column=4, row=1, padx=pad)
-    quitButton = tk.Button(buttonFrame, text='QUIT', width=bWidth)
-    quitButton.grid(column=5, row=1, padx=pad)
-    mainWindow.mainloop()
-
-def GetPageNumber():
-    global currentPos
-    currentPos = tk.simpledialog.askinteger('Page Number', 'Enter page number')
-
+        bWidth = 8
+        pad=2
+        top = tk.Button(buttonFrame, command=lambda : changePageNumber(0, text, mainWindow), text='TOP', width=bWidth)
+        top.grid(column=0, row=1, padx=pad)
+        up = tk.Button(buttonFrame, command=lambda : changePageNumber(currentPos-1, text, mainWindow), text='UP', width=bWidth)
+        up.grid(column=1, row=1, padx=pad)
+        down = tk.Button(buttonFrame, command=lambda : changePageNumber(currentPos+1, text, mainWindow), text='DOWN', width=bWidth)
+        down.grid(column=2, row=1, padx=pad)
+        bottom = tk.Button(buttonFrame, command=lambda : changePageNumber(len(offsets)-1, text, mainWindow), text='BOTTOM', width=bWidth)
+        bottom.grid(column=3, row=1, padx=pad)
+        page = tk.Button(buttonFrame, command=lambda : getPageNumber(text, mainWindow), text='PAGE', width=bWidth)
+        page.grid(column=4, row=1, padx=pad)
+        quitButton = tk.Button(buttonFrame, command=lambda : sys.exit(), text='QUIT', width=bWidth)
+        quitButton.grid(column=5, row=1, padx=pad)
+        mainWindow.mainloop()
+            
 def main():
     if len(sys.argv) > 2:
         view(sys.argv[1], sys.argv[2])
